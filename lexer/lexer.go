4840c6156a6c835695330ecb73b3da66fa14c5da
Mohamed taha Baz <mobaz@e1r8p2.1337.ma>
2020-11-16 12:13:12 +0100

    add NextToken method and pass the first test

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,6 @@
 package lexer
 
+import "token"
 type Lexer struct {
 	input			string
 	position		int
@@ -22,6 +23,33 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
-func New(input string) *Lexer {
-	l := &Lexer{input: input}
+func (l *Lexer) NextToken() token.Token { 
+	var tok token.Token
+	l.readChar()
+	switch l.ch { 
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch) 
+	case '+':
+		tok = newToken(token.PLUS, l.ch) 
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch) 
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	return tok 
+}
+
+func newToken(tokenType string, ch byte) token.Token { 
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
