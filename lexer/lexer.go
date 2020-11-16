9e98f501bcb23103cdafeaeed71dadebf2c6255a
Mohamed taha Baz <mobaz@e1r8p2.1337.ma>
2020-11-16 13:32:14 +0100

    check let and fn keywords

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -26,6 +26,7 @@ func (l *Lexer) readChar() {
 func (l *Lexer) NextToken() token.Token { 
 	var tok token.Token
 	l.readChar()
+	l.skipWhitespace()
 	switch l.ch { 
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -46,10 +47,36 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		}
+		tok = newToken(token.ILLEGAL, l.ch)
 	}
 	return tok 
 }
 
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+
 func newToken(tokenType string, ch byte) token.Token { 
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
--- token/token.go
+++ token/token.go
@@ -21,3 +21,25 @@ const (
 	FUNCTION	= "FUNCTION"
 	LET			= "LET"
 )
+
+var t = []string{"let", "fn"}
+
+func LookupIdent(ident string) string {
+	for i := 0; i < len(t); i++ {
+		if ident == t[i] {
+			return getKeyType(ident)
+		}
+	}
+	return IDENT
+}
+
+func getKeyType(ident string) string {
+	var key string
+	switch ident {
+	case "let":
+		key = LET
+	case "fn":
+		key = FUNCTION
+	}
+	return key
+}
