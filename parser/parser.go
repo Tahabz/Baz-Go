29635860aa4af0b74fad2e1d916b97068983c572
Mohamed taha Baz <mobaz@e1r9p12.1337.ma>
2020-11-20 12:56:18 +0100

    add letStatement parser check

--- parser/parser.go
+++ parser/parser.go
@@ -4,17 +4,22 @@ import (
 	"ast"
 	"lexer"
 	"token"
+	"fmt"
 )
 
 type Parser struct {
 	l *lexer.Lexer
 
+	errors []string
 	curToken token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l: l,
+		errors: []string{},
+	}
 	p.nextToken()
 	p.nextToken()
 	return p
@@ -23,6 +28,16 @@ func New(l *lexer.Lexer) *Parser {
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken();
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t string) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
@@ -49,5 +64,37 @@ func (p *Parser) parseStatement() ast.Statement {
 }
 
 func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
 	
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken();
+	}
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t string) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) expectPeek(t string) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+	p.peekError(t)
+	return false
+}
+
+func (p *Parser) peekTokenIs(t string) bool {
+	return p.peekToken.Type == t
 }
